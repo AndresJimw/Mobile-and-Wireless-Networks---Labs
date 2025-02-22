18f9fe9f2b2848f3402d7bf2a5dd7534f0174102
AndresJimw <brandrez.474@gmail.com>
2025-02-21 23:51:59 -0500

    refactor: update Exercise 0 script in Lab 01 with detailed comments and improved plotting

--- Lab01/Excercise_0.m
+++ Lab01/Excercise_0.m
@@ -1,7 +1,16 @@
-t = 0:0.01:10; % Data from 0 to 10 seconds
-y = 5 * sin(t); % Evaluates function at each time in t
+% Notes:
+% - Continuous-time signals are defined for every instant of time.
+% - The 'plot' function is used to visualize continuous-time signals in MATLAB.
+% - Understanding how to create and plot continuous-time signals is essential for signal processing.
 
+% Define Time Vector
+t = 0:0.01:10; % Data from 0 to 10 seconds, with an increment of 0.01 seconds
+
+% Define Continuous-Time Signal
+y = 5 * sin(t); % Evaluates the sine function at each time in t
+
+% Plot the Continuous-Time Signal
 plot(t, y);
-title('5sin(t)');
+title('5sin(t)'); 
 xlabel('time (sec)');
-grid;
+grid; 
